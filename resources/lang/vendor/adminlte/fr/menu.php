9728ce8002dfbeeb66d2f4ce785e6bed6f1d3974
Jordi Serrano <93100971+jordiserrano7@users.noreply.github.com>
2022-06-13 22:34:04 +0200

    no message

---
+++ resources/lang/vendor/adminlte/fr/menu.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'main_navigation'               => 'MENU MAJEUR',
+    'blog'                          => 'Blog',
+    'pages'                         => 'Page',
+    'account_settings'              => 'PARAMÈTRES DU COMPTE',
+    'profile'                       => 'Mon profil',
+    'shopping_cart'                 => 'Panier',
+    'administration'                => 'La gestion',
+    'categories'                    => 'Catégories',
+    'products'                      => 'Produits',
+    'inventory'                     => 'Inventaire',
+    'sizes'                         => 'Dimensionnement',
+    'labels'                        => 'ÉTIQUETTES',
+    'important'                     => 'Important',
+    'warning'                       => 'Mise en garde',
+    'information'                   => 'Informations',
+];
