b7a909ebf931838638cb375110cc71c428516b6a
Jordi Serrano <93100971+jordiserrano7@users.noreply.github.com>
2022-06-13 14:32:51 +0200

    no message

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Ecommerce Laravel Project')
+
+@section('content_header')
+    <h1>Ecommerce Laravel Project</h1>
+@stop
+
+@section('content')
+    <p>Ecommerce Laravel Project welcome:)</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
