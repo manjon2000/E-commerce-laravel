1d46162e4cb7678054808f5119d9b29d2fca9414
Nil <nilriera2000@gmail.com>
2022-06-14 19:39:50 +0200

    mini fix

--- resources/views/backend/categories/detail.blade.php
+++ resources/views/backend/categories/detail.blade.php
@@ -3,48 +3,46 @@
 @section('title', 'E-commerce - Categories')
 
 @section('content_header')
-    <h1>{{ __('web.categories') }} | {{ __('web.detail') }}</h1>
+    <h1>{{__("web.categories")}} | {{__("web.list")}}</h1>
 @stop
 
 @section('content')
 
-    <a href="{{ route('categories.create') }}"><button class="btn btn-success">{{ __('web.create') }}</button></a>
+<a href="{{ route('categories.create') }}"><button class="btn btn-success">{{ __("web.create")}}</button></a>
+<a href="{{route("categories.index")}}">{{__("web.return")}}</a>
 
-    <div class="container">
+<div class="container">
 
-        <table class="table table-bordered">
-            <tr>
-                <th>{{ __('web.name') }}</th>
-                <th>ES</th>
-                <th>EN</th>
-                <th>FR</th>
-                <th>img</th>
-                <th>{{ __('web.tools') }}</th>
-            </tr>
+    <table class="table table-bordered">
+        <tr>
+            <th>{{ __("web.name")}}</th>
+            <th>ES</th>
+            <th>EN</th>
+            <th>FR</th>
+            <th>img</th>
+            <th>{{ __("web.tools")}}</th>
+        </tr>
             <tr>
                 <td>{{ $category->name }}</td>
-                {{ App::setLocale('es') }}
-                <td>{{ $category->description }}</td>
-                {{ App::setLocale('en') }}
-                <td>{{ $category->description }}</td>
-                {{ App::setLocale('fr') }}
-                <td>{{ $category->description }}</td>
-                <td><img src="{{ asset('images/' . $category->image_category) }}" alt="" width="100px"></td>
+                <td>{{ $category->translate('es')->description }}</td>
+                <td>{{ $category->translate('en')->description }}</td>
+                <td>{{ $category->translate('fr')->description }}</td>
+                <td><img src="{{asset("images/".$category->image_category)}}" alt="" width="100px"></td>
                 <td>
-                    <a href="{{ route('categories.edit', ['category' => $category->id]) }}"><button
-                            class="btn btn-secondary">{{ __('web.edit') }}</button></a>
-                    {!! Form::open(['url' => route('categories.destroy', ['category' => $category->id]), 'method' => 'POST']) !!}
-                    @csrf
-                    @method('DELETE')
-                    {!! Form::submit(__('web.delete'), ['class' => 'btn btn-danger']) !!}
-
-                    {!! Form::close() !!}
-                </td>
+                    <a href="{{ route('categories.show',['category' => $category->id]) }}"><button class="btn btn-primary">{{__("web.detail")}}</button></a>
+                <a href="{{ route('categories.edit', ['category' => $category->id]) }}"><button class="btn btn-secondary">{{__("web.edit")}}</button></a>
+                {!! Form::open(['url' => route('categories.destroy',['category' => $category->id]), 'method' => 'POST']) !!}
+                @csrf
+                @method('DELETE')
+                {!! Form::submit(__("web.delete"), ["class"=>"btn btn-danger"]) !!}
+    
+                {!! Form::close() !!}
+            </td>
 
             </tr>
-
-        </table>
-    </div>
+    
+    </table>
+</div>
 
 @stop
 
@@ -53,7 +51,9 @@
 @stop
 
 @section('js')
-    <script>
-        console.log('Hi!');
-    </script>
+    <script> console.log('Hi!'); </script>
 @stop
+
+
+
+
