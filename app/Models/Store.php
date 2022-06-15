d69f73ea0f96f71e28e66cc43071180421b2b759
Nil <nilriera2000@gmail.com>
2022-06-15 22:37:20 +0200

    fix conflict

--- app/Models/Store.php
+++ app/Models/Store.php
@@ -6,12 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Store extends Model
 {
-<<<<<<< HEAD
-    protected $fillable = ['name', 'address', 'phone_number', 'email', 'city_id', 'schedule'];
-=======
     protected $fillable = ['name', 'address', 'phone_number', 'email', 'city_id',"schedule_end", "schedule_start"];
     public function inventories(){
         return $this->belongsTo('App\Models\Inventory', 'store_id', 'id');
     }
->>>>>>> main
 }
