ee1c6cde602b2ee533707214e750ae9bc71d37ab
Jordi Serrano <93100971+jordiserrano7@users.noreply.github.com>
2022-06-15 13:25:59 +0200

    no message

--- resources/views/backend/products/index.blade.php
+++ resources/views/backend/products/index.blade.php
@@ -37,35 +37,31 @@
                             <label for="price" class="form-label ml-5">{{__('web.price_product')}}</label>
                         </div>
                         <div class="mb-3 col-6">
-                            <label name="price" class="form-label">{{$user->email}}</label>
+                            <label name="price" class="form-label">{{$product->price}}</label>
                         </div>
                         <!-- Categoria -->
                         <div class="mb-3 col-6 vvv">
-                            <label for="username" class="form-label ml-5">{{__('web.')}}</label>
+                            <label for="username" class="form-label ml-5">{{__('web.name_category_product')}}</label>
                         </div>
                         <div class="mb-3 col-6">
-                            <label name="username" class="form-label">@if(is_null($user->last_name)){{__('web.not-data')}} @else{{$user->last_name}}@endif</label>
+                            <label name="username" class="form-label">{{$product->categories->name}}</label>
                         </div>
                         <!-- Imagen Principal -->
                         <div class="mb-3 col-6 vvv">
                             <label for="main_image" class="form-label ml-5">{{__('web.principal_image_products')}}</label>
                         </div>
-                        <div class="mb-3 col-6">
-                            <img name="main_image" src="{{asset('$product->image_product')}}" alt="Main image">
-                        </div>
-                </div>
-            </div>
-    @endif
-            <!-- MODIFICAR -->
-            <div class="container border border-dark d-flex justify-content-center rounded-bottom shadow-lg p-3 bg-white">
-                <div class="row">
-                    <div class="mb-3 mt-3 col-12">
-                        <a class="btn btn-dark btn-lg" 
-                        href="{{route('profiles.edit', ['profile'=>$user->id])}}">
-                        {{__('web.edit_profile')}}</a>
-                    </div>
+                        @if(is_null($product->image_product))
+                            <i class="far fa-images fs-2"></i>
+                        @else
+                            <div class="mb-3 col-6">
+                                <img name="main_image" src="{{asset('$product->image_product')}}" alt="Main image">
+                            </div>
+                        @endif
+
                 </div>
             </div>
             @endif
             @endforeach
+    @endif
+            
 @endsection
